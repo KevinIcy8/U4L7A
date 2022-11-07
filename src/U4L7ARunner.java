518382c636872cdea4af344d11582b9584785f2d
BT_4N2_17 <lsarmiento9311@bths.edu>
2022-11-07 12:34:24 -0500

    Initial commit

---
+++ src/U4L7ARunner.java
@@ -0,0 +1,15 @@
+public class U4L7ARunner {
+    public static void main(String[] args) {
+        for (int j = 0; j < 3; j++)
+        {
+            int i = 1;
+            while (i < 4)
+            {
+                System.out.println(j + i);
+                i++;
+            }
+            System.out.println();
+        }
+
+    }
+}
